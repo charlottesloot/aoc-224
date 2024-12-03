cea5ebc2629813d365d6bf5c1f2f532919b68482
Charlotte Slootmaeckers <charlotte@imengine.be>
2024-12-03 13:12:46 +0100

    day 3 part 2

--- day_03/index.ts
+++ day_03/index.ts
@@ -3,12 +3,22 @@ const input = readFileSync('input', "utf-8");
 // npx ts-node index.ts 
 
 const regex = /mul\([0-9]{1,3},[0-9]{1,3}\)/g;
-const matches = input.match(regex) || [];
+const extendedRegex = /(mul\((\d+),(\d+)\)|do\(\)|don't\(\))/g;
+const matches = input.match(extendedRegex) || [];
 let total = 0
-
+let enabled : boolean = true
 for (let index = 0; index < matches.length; index++) {
     const element = matches[index];
-    let [x,y] = element.substring(4, element.length - 1).split(',')
+    if(element === 'do()') {
+        enabled = true; 
+        continue;
+    }
+    if(element.includes('don')) {
+        enabled = false; 
+        continue;
+    }
+    if(!enabled) continue;
+    let [x,y] = element.substring(4, element.length - 1).split(',');
     total += (parseInt(x) * (parseInt(y)))
 }
 
