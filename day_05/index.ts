68dc89997ee46a684c0506b874625f697de030dd
Charlotte Slootmaeckers <charlotte@imengine.be>
2024-12-06 14:19:04 +0100

    day 5 part 2

--- day_05/index.ts
+++ day_05/index.ts
@@ -5,7 +5,7 @@ const set = Array.from(new Set(rules.flat()));
 // npx ts-node index.ts 
 
 console.log(rules)
-console.log(updates)
+// console.log(updates)
 
 let total = 0;
 
@@ -14,14 +14,7 @@ for (let index = 0; index < updates.length; index++) {
     const update = updates[index]
     let valid = true;
 
-    console.log('---------', update,'------------')
-
-    // const valid = set.every(pagenum => update.includes(pagenum));
-    // console.log('valid', valid);
-
-    // x = vind index van rule[0] in update. 
-    // y = vind index van rule[1] in update. 
-    // als x < y  ? VALID  : INVALID  + skip next
+    console.log('---------UPDATE------------')
 
     for (let i = 0; i < rules.length; i++) {
         if(!valid) break;
@@ -29,23 +22,55 @@ for (let index = 0; index < updates.length; index++) {
         const rule = rules[i];
         const [first, second] = [update.indexOf(rule[0]), update.indexOf(rule[1])]
 
-        console.log('VALID rule check ', rule, ' // first : ', first, ' - second : ', second);
-
         if(first === -1 || second === -1 ) continue;
         if(first < second) continue;
 
         valid = false;
     }
     
-    if(valid){
-        console.log('valid! ', update);
-    } else {
-        console.log('not valid;;; ', update);
-    }
-    if(!valid) continue;
+    if(valid) continue;
+    
+    
+    // sort correctly
+    let updateSorted = structuredClone(update);
+    console.log('====== not valid ========= ', updateSorted);
+    console.table(rules)
+    
+    updateSorted.sort((a,b) => {
+        let swap = false;
+
+        // 
+        for (let i = 0; i < rules.length; i++) {  
+
+            const [validFirstIndex, validSecondIndex] = [update.indexOf(rules[i][0]), update.indexOf(rules[i][1])]
+            // console.log('test', validFirstIndex, validSecondIndex), b , a;
+
+            if(validFirstIndex !== -1 && validSecondIndex !== -1) {
+                // SWAP CHECK
+
+                console.log('SWAP CHECK : current rule', rules[i], ' / current a b ', b, a, 'valid indexes 1 2 ' , validFirstIndex, validSecondIndex);
+
+                if(a === rules[i][0] && b === rules[i][1]) {
+                    swap = true;
+                    console.log('SWAP!', b, a, ' / to become / ', rules[i]);
+                    break;
+                }
+
+            } else continue;
+
+        }
+       
+        console.log('swap? ', swap, ' // ' ,b, a)
+        return swap ? -1 : 0
+    })
+
+    
+
+
+    console.log('reworked  ', updateSorted);
 
     // get middle number
-    total += update.at(update.length/2) || 0;
+    total += updateSorted.at(updateSorted.length/2) || 0;
     
 }
 
