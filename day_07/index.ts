e3d6389396efd74bc41b7eda64580805b28ee8bd
Charlotte Slootmaeckers <charlotte@imengine.be>
2024-12-11 15:50:51 +0100

    day 7 part 2

--- day_07/index.ts
+++ day_07/index.ts
@@ -1,5 +1,5 @@
 import { readFileSync } from "fs";
-let data = readFileSync('testinput', "utf-8").split('\n')?.map(x => x.split(':'));
+let data = readFileSync('input', "utf-8").split('\n')?.map(x => x.split(':'));
 // npx ts-node index.ts 
 // const operators = ['+', '*'];
 const operators = ['+', '*', '||'];
@@ -14,15 +14,33 @@ function checkIfThereIsValidOperatorCombinationResult(equation: number[], calibr
         if (index === equation.length - 1) {
             return currentValue === calibrationResult
         }
+        
+        if (recursive(index + 1, currentValue + equation[index + 1])) return true; // Stop further recursion if a valid result is found
+        if (recursive(index + 1, currentValue * equation[index + 1])) return true; // Stop further recursion if a valid result is found
 
-        for (const operator of operators) {
-            if (operator === "+") {
-                if (recursive(index + 1, currentValue + equation[index + 1])) return true; // Stop further recursion if a valid result is found
-                
-            } else if (operator === "*") {
-                if (recursive(index + 1, currentValue * equation[index + 1])) return true; // Stop further recursion if a valid result is found
-            } 
+        return false; // No valid combination found
+    }
+
+    return recursive(0, equation[0]);
+}
+
+
+function checkIfThereIsValidOperatorCombinationResult2(equation: number[], calibrationResult: number) : boolean {
+    let equasionBackup = structuredClone(equation);
+    
+    const recursive = (index : number, currentValue: number) => {
+        // save result
+        if (index === equation.length - 1) {
+            return currentValue === calibrationResult
         }
+
+
+        if (recursive(index + 1, currentValue + equation[index + 1])) return true; // +
+        if (recursive(index + 1, currentValue * equation[index + 1])) return true;  // *
+        
+        // "||" operator (combine current and next numbers)
+        const combinedValue = Number(`${currentValue}${equation[index + 1]}`);
+        if (recursive(index + 1, combinedValue)) return true;
 
         return false; // No valid combination found
     }
@@ -34,7 +52,7 @@ for (let index = 0; index < data.length; index++) {
     const result = Number(data[index][0]);
     const operation = data[index][1].split(' ').map(x => Number(x));
     
-    const x = checkIfThereIsValidOperatorCombinationResult(operation, result);
+    const x = checkIfThereIsValidOperatorCombinationResult2(operation, result);
     if(x) calibrationResult += result;
     // console.log(data[index] , ` ::: `, x)
 
